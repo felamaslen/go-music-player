bc244d11386f7a63b998207236eeda2f38cf9535
Fela Maslen <felamaslen@gmail.com>
2020-12-12 00:37:15 +0000

    chore: moved migrations directory to root of project

--- music-player/cmd/migrate/main.go
+++ music-player/cmd/migrate/main.go
@@ -1,29 +1,9 @@
 package main
 
 import (
-  "os"
-  "log"
-  "fmt"
-  "path/filepath"
-
-  "github.com/felamaslen/go-music-player/pkg/config"
-
-  migrate "github.com/golang-migrate/migrate/v4"
-  _ "github.com/golang-migrate/migrate/v4/database/postgres"
-  _ "github.com/golang-migrate/migrate/v4/source/file"
+  "github.com/felamaslen/go-music-player/pkg/db"
 )
 
 func main() {
-  databaseUrl := fmt.Sprintf("%s?sslmode=disable", config.Config.DatabaseUrl)
-  cwd, err := os.Getwd()
-  if err != nil {
-    log.Fatal("Error getting working dir: ", err)
-  }
-  directoryUrl := fmt.Sprintf("file://%s", filepath.Join(cwd, "pkg/db/migrations"))
-
-  m, err := migrate.New(directoryUrl, databaseUrl)
-  if err != nil {
-    log.Fatal("Error setting up: ", err)
-  }
-  m.Up()
+  db.MigrateDatabase()
 }
